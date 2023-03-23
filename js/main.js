d2186d284c99bce1dd91d1fac865d2f98932cd6e
haoyuwei <1336630591@qq.com>
2023-03-23 16:24:12 +0800

    开启延时

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var LWidth = document.documentElement.clientWidth-20;
 var LHeight = document.documentElement.clientHeight-20;
 
 $(".banner").css({width:LWidth, height:LHeight});
-$(".banner .img").css({width:LWidth*7, height:LHeight});
+$(".banner .img").css({width:LWidth*7, height:LHeight,left:-LWidth});
 $(".banner .img img").css({width:LWidth, height:LHeight});
 
 
@@ -18,18 +18,23 @@ var $div_img_width =$(".banner .img").width()/7;
 
 //轮播定时器
 
-var timeer = setInterval(changeImage,2000);
+var timeer = setInterval(changeImage,2500);
 function changeImage(){
     index++;
     $(".indicator span").removeClass("cur");
     $(".indicator span").eq(index-1).addClass("cur");
-
+    if(index == 6){
+        $(".indicator span").removeClass("cur");
+        $(".indicator span").eq(0).addClass("cur");
+    }
     $div_img.finish().animate({
-        left :-$div_img_width*(index-1)
+        left :-$div_img_width*index
     },1000,function(){
-        if(index >= 7){
+        if(index >= 6){
             index =0;
-            $(this).css("left",-$div_img_width*index);
+            $(this).css("left",-$div_img_width);
+            $(".indicator span").removeClass("cur");
+            $(".indicator span").eq(index).addClass("cur");
         }
     });
 }
@@ -40,7 +45,7 @@ $(".page span").hover(function(){
     clearInterval(timeer);
 },function(){
     //继续执行
-    timeer = setInterval(changeImage,2000);
+    timeer = setInterval(changeImage,2500);
 });
 
 
@@ -55,15 +60,15 @@ $(".page .prev").click(function(){
             left :-$div_img_width*index
         },500,function(){
             if(index <=0){
-                index =7;
-                $(this).css("left",-$div_img_width*(index-1));
+                index =5;
+                $(this).css("left",-$div_img_width*index);
             }
         });	
 });
 //页面指示器
 $(".indicator span").mouseenter(function(){
     //切换图片
-    index= $(this).index() +1;
+    index= $(this).index()+1;
     clearInterval(timeer);
-    $div_img.css("left",-$div_img_width*(index-1));
+    $div_img.css("left",-$div_img_width*index);
 });
